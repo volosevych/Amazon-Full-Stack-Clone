e5b88ed3fcef7ec529799ff420c92f685ca6ef0e
Anastasia Volosevych <volosevychh@gmail.com>
2021-09-11 16:01:43 -0600

    adding some props

--- amazon-clone/src/components/Home/Home.js
+++ amazon-clone/src/components/Home/Home.js
@@ -6,18 +6,21 @@ const Home = () => {
     return (
         <div className="home">
             <div className="home__container">
-                <img className="home__img" src="https://m.media-amazon.com/images/I/615hfyTPlYL._SX3000_.jpg" alt="" />
+                <img className="home__img" src="https://m.media-amazon.com/images/I/615hfyTPlYL._SX3000_.jpg" alt="Intel Core i7" />
 
                 <div className="home__row">
-                    <Product />
+                    <Product title="Intel Core i7-10700KF" price={299.99} image="https://m.media-amazon.com/images/I/714-1OvyFIL._AC_SL1500_.jpg" rating={4} />
+                    <Product title="Roborock S7+ Robot Vacuum and Sonic Mop" price={750.00} rating={4} id="" image="https://m.media-amazon.com/images/I/513EduvbsNL._AC_SL1500_.jpg" />
                 </div>
 
                 <div className="home__row">
-
+                    <Product title="DEWALT 20V MAX" price={179.00} rating={5} id="" image="https://m.media-amazon.com/images/I/81jUpxvLwXL._AC_SL1500_.jpg" />
+                    <Product title="Minka-Aire F896-65-BNW" price={469.95} rating={4} id="" image="https://m.media-amazon.com/images/I/91S1PIX+yWL._AC_SL1500_.jpg" />
+                    <Product title="Brita Standard Everyday Water Filter" price={26.99} rating={4} id="" image="https://m.media-amazon.com/images/I/51qaJZPphuS._AC_SL1200_.jpg" />
                 </div>
 
                 <div className="home__row">
-                    
+                    <Product title="SAMSUNG (MZ-V8V1T0B/AM)" price={129.99} rating={5} id="" image="https://m.media-amazon.com/images/I/41JykwmoA2L._AC_.jpg" />
                 </div>
             </div>
         </div>
--- amazon-clone/src/components/Product/Product.js
+++ amazon-clone/src/components/Product/Product.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from 'react';
+import StarIcon from '@material-ui/icons/Star';
 import './Product.css'
 
-const Product = () => {
+const Product = ({ id, title, image, price, rating }) => {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The Lean Startup</p>
+                <a href="google.com">{title}</a>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
 
                 <div className="product__rating">
-                    <p>5</p>
+                    {/* cool trick */}
+                    {Array(rating).fill().map((_, i) => (
+                        <StarIcon className="product__star"/>
+                    ))}
                 </div>
             </div>
 
-            <img src="" alt=""/>
+            <img src={image} alt=""/>
+
+            <button>Add to Basket</button>
         </div>
     )
 }
